2585a21a6dcc7024511ef78b886afea095ace940
nexteacc <nexteacc@gmail.com>
2025-12-28 16:34:11 +0700

    Add publicDir configuration to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      publicDir: 'public',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
